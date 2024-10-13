093703b5a06006d4b6e2ff375ff61eca8d9c4949
Daniel Moffett <moffd234@gmail.com>
2024-10-13 12:53:22 -0400

    Started transforming if statements to a hashmap approach

--- src/main/java/org/example/TextEditor.java
+++ src/main/java/org/example/TextEditor.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 //TextEditor class starts here
 class TextEditor extends Frame implements ActionListener {
+    private HashMap<String, Runnable> actionMap = new HashMap<String, Runnable>();
     TextArea ta = new TextArea();
     int i, len1, len, pos1;
     String str = "";
@@ -26,6 +27,7 @@ class TextEditor extends Frame implements ActionListener {
         setLayout(new BorderLayout());
         add("Center", ta);
         setMenuBar(mb);
+
         Menu m1 = new Menu("File");
         Menu m2 = new Menu("Edit");
         Menu m3 = new Menu("Tools");
@@ -34,19 +36,24 @@ class TextEditor extends Frame implements ActionListener {
         mb.add(m2);
         mb.add(m3);
         mb.add(m4);
+
         MenuItem[] mi1 = {
                 new MenuItem("New"), new MenuItem("Open"), new MenuItem("Save"), new MenuItem("Save As"),
                 new MenuItem("Page Setup"), new MenuItem("Print"), new MenuItem("Exit")
         };
+
         MenuItem[] mi2 = { new MenuItem("Delete"), new MenuItem("Cut"),
                 new MenuItem("Copy"), new MenuItem("Paste"), new MenuItem("Find"),
                 new MenuItem("Find Next"), new MenuItem("Replace"),
                 new MenuItem("Go To"), new MenuItem("Select All"),
                 new MenuItem("Time Stamp") };
+
         MenuItem[] mi3 = { new MenuItem("Choose Font"), new MenuItem("Compile"),
                 new MenuItem("Run") };
+
         MenuItem[] mi4 = { new MenuItem("Help Topics"),
                 new MenuItem("About TextEditor") };
+
         for (MenuItem menuItem : mi1) {
             m1.add(menuItem);
             menuItem.addActionListener(this);
@@ -65,6 +72,8 @@ class TextEditor extends Frame implements ActionListener {
             m4.add(menuItem);
             menuItem.addActionListener(this);
         }
+
+        populateMappings();
         MyWindowsAdapter mw = new MyWindowsAdapter(this);
         addWindowListener(mw);
         setSize(500, 500);
@@ -72,90 +81,69 @@ class TextEditor extends Frame implements ActionListener {
         setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent ae) {
-        String arg = ae.getActionCommand();
-        if (arg.equals("New")) {
-            dispose();
-            TextEditor t11 = new TextEditor();
-            t11.setSize(500, 500);
-            t11.setVisible(true);
-        }
-        try {
-            if (arg.equals("Open")) {
-                FileDialog fd1 = new FileDialog(this, "Select File", FileDialog.LOAD);
-                fd1.setVisible(true);
-                StringBuilder s4 = new StringBuilder();
-                s2 = fd1.getFile();
-                s3 = fd1.getDirectory();
-                s32 = s3 + s2;
-                File f = new File(s32);
-                FileInputStream fii = new FileInputStream(f);
-                len = (int) f.length();
-                for (int j = 0; j < len; j++) {
-                    char s5 = (char) fii.read();
-                    s4.append(s5);
-                }
-                ta.setText(s4.toString());
-                fii.close();
-            }
-        } catch (IOException ignored) {
-        }
-        try {
-            if (arg.equals("Save As")) {
-                FileDialog dialog1 = new FileDialog(this, "Save As", FileDialog.SAVE);
-                dialog1.setVisible(true);
-                s7 = dialog1.getDirectory();
-                s8 = dialog1.getFile();
-                s9 = s7 + s8 + ".txt";
-                s6 = ta.getText();
-                len1 = s6.length();
-                byte[] buf = s6.getBytes();
-                File f1 = new File(s9);
-                FileOutputStream fOutputStream = new FileOutputStream(f1);
-                for (int k = 0; k < len1; k++) {
-                    fOutputStream.write(buf[k]);
-                }
-                fOutputStream.close();
-            }
-            this.setTitle(s8 + " TextEditor File");
-        } catch (IOException ignored) {
-        }
-        if (arg.equals("Exit")) {
-            System.exit(0);
-        }
-        if (arg.equals("Cut")) {
-            str = ta.getSelectedText();
-            i = ta.getText().indexOf(str);
-            ta.replaceRange(" ", i, i + str.length());
-        }
-        if (arg.equals("Copy")) {
-            str = ta.getSelectedText();
-        }
-        if (arg.equals("Paste")) {
-            pos1 = ta.getCaretPosition();
-            ta.insert(str, pos1);
-        }
-        if (arg.equals("Delete")) {
-            String msg = ta.getSelectedText();
-            i = ta.getText().indexOf(msg);
-            ta.replaceRange(" ", i, i + msg.length());
-        }
-        if (arg.equals("Select All")) {
-            String strText = ta.getText();
-            int strLen = strText.length();
-            ta.select(0, strLen);
-        }
-        if (arg.equals("Time Stamp")) {
-            String hms = getHMS();
-            int loc = ta.getCaretPosition();
-            ta.insert(hms, loc);
-        }
-        if (arg.equals("About TextEditor")) {
-            AboutDialog d1 = new AboutDialog(this, "About TextEditor");
-            d1.setVisible(true);
-            setSize(500, 500);
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        handleCommand(command);
+    }
+
+    private void handleCommand(String command) {
+        Runnable action = actionMap.get(command);
+        if (action != null) {
+            action.run();  // Executes the corresponding action
+        } else {
+            System.out.println("Unknown command: " + command);
         }
     }
+
+//    public void actionPerformed(ActionEvent ae) {
+//        String arg = ae.getActionCommand();
+//        if (arg.equals("New")) {
+//            dispose();
+//            TextEditor t11 = new TextEditor();
+//            t11.setSize(500, 500);
+//            t11.setVisible(true);
+//        }
+//        try {
+//            if (arg.equals("Open")) {
+//                FileDialog fd1 = new FileDialog(this, "Select File", FileDialog.LOAD);
+//                fd1.setVisible(true);
+//                StringBuilder s4 = new StringBuilder();
+//                s2 = fd1.getFile();
+//                s3 = fd1.getDirectory();
+//                s32 = s3 + s2;
+//                File f = new File(s32);
+//                FileInputStream fii = new FileInputStream(f);
+//                len = (int) f.length();
+//                for (int j = 0; j < len; j++) {
+//                    char s5 = (char) fii.read();
+//                    s4.append(s5);
+//                }
+//                ta.setText(s4.toString());
+//                fii.close();
+//            }
+//        } catch (IOException ignored) {
+//        }
+//        try {
+//            if (arg.equals("Save As")) {
+//                FileDialog dialog1 = new FileDialog(this, "Save As", FileDialog.SAVE);
+//                dialog1.setVisible(true);
+//                s7 = dialog1.getDirectory();
+//                s8 = dialog1.getFile();
+//                s9 = s7 + s8 + ".txt";
+//                s6 = ta.getText();
+//                len1 = s6.length();
+//                byte[] buf = s6.getBytes();
+//                File f1 = new File(s9);
+//                FileOutputStream fOutputStream = new FileOutputStream(f1);
+//                for (int k = 0; k < len1; k++) {
+//                    fOutputStream.write(buf[k]);
+//                }
+//                fOutputStream.close();
+//            }
+//            this.setTitle(s8 + " TextEditor File");
+//        } catch (IOException ignored) {
+//        }
+//    }
 
     private String getHMS() {
         GregorianCalendar gCalendar = new GregorianCalendar();
@@ -168,6 +156,46 @@ class TextEditor extends Frame implements ActionListener {
         return "Time" + " - " + h + ":" + m + ":" + s + " Date" + " - " + date + " " + mon + " " + year + " ";
     }
 
+    private void populateMappings(){
+        actionMap.put("Exit", () -> System.exit(0));
+
+        actionMap.put("Cut", () -> {
+            str = ta.getSelectedText();
+            i = ta.getText().indexOf(str);
+            ta.replaceRange(" ", i, i + str.length());
+        });
+
+        actionMap.put("Copy", () -> str = ta.getSelectedText());
+
+        actionMap.put("Paste", () -> {
+            pos1 = ta.getCaretPosition();
+            ta.insert(str, pos1);
+        });
+
+        actionMap.put("Delete", () -> {
+            String msg = ta.getSelectedText();
+            i = ta.getText().indexOf(msg);
+            ta.replaceRange(" ", i, i + msg.length());
+        });
+
+        actionMap.put("Select All", () -> {
+            String strText = ta.getText();
+            int strLen = strText.length();
+            ta.select(0, strLen);
+        });
+
+        actionMap.put("Time Stamp", () -> {
+            String hms = getHMS();
+            int loc = ta.getCaretPosition();
+            ta.insert(hms, loc);
+        });
+
+        actionMap.put("About TextEditor", () -> {
+            AboutDialog d1 = new AboutDialog(this, "About TextEditor");
+            d1.setVisible(true);
+            setSize(500, 500);
+        });
+    }
 }
 
 
